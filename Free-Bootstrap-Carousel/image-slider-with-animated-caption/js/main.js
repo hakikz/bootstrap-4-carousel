2301c8bcf45aaab338f50316a11e1dc3ce31c2d2
Hakik Zaman <hakikzaman@gmail.com>
2018-07-10 15:35:23 +0600

    Add files via upload

---
+++ Free-Bootstrap-Carousel/image-slider-with-animated-caption/js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+            
+    $(".carousel-caption h5").addClass('animated slideInLeft');
+    
+    $(".carousel-caption p").addClass('animated slideInRight');
+    
+});
---
+++ Free-Bootstrap-Carousel/slider-bottom-to-top/js/main.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+            
+    $(".carousel-item").addClass('animated slideInUp');
+    
+});
